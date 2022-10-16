0d0425099c5c02a948f8285d8b9d9d7659015fa8
hbc8141 <hbc8141@naver.com>
2022-10-16 19:41:11 +0900

    create FTP
    create FTP

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path');
+const edge = require('edge-js');
+
+const dllPath = path.join(__dirname, './lib/dll/release/FTPLibrary.dll');
+
+const dllLib = {};
+
+/**
+ * Method: Create local ftp
+ * @param {string} role ftp user rold
+ * @param {string} siteName ftp site name
+ * @param {string} serverIp  ftp server ip
+ * @param {string} physicalPath file path of ftp server
+ */
+dllLib.ftp = edge.func({
+    assemblyFile: dllPath,
+    typeName: 'Ftp.Ftp',
+    methodName: 'AddFtp'
+})
---
+++ lib/dll/src/FTPLibrary/Class1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Web.Administration;
+
+namespace FTPLibrary
+{
+    public class FTP
+    {
+        /// <summary>
+        /// FTP 추가
+        /// </summary>
+        /// <param name="role">유저명</param>
+        /// <param name="siteName">사이트이름</param>
+        /// <param name="serverIp">IP주소</param>
+        /// <param name="physicalPath">경로</param>
+        /// <returns></returns>
+        public static void AddFtp(string role, string siteName = "localhost", string serverIp = "127.0.0.1", string physicalPath = @"C:\ftp")
+        {
+            using (ServerManager serverManager = ServerManager.OpenRemote(serverIp))
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+                ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+
+                ConfigurationElement siteElement = sitesCollection.CreateElement("site");
+                siteElement["name"] = @"localhost";
+                siteElement["id"] = 3;
+                siteElement["serverAutoStart"] = true;
+
+                ConfigurationElementCollection bindingsCollection = siteElement.GetCollection("bindings");
+                ConfigurationElement bindingElement = bindingsCollection.CreateElement("binding");
+                bindingElement["protocol"] = @"ftp";
+                bindingElement["bindingInformation"] = @"*:21:";
+                bindingsCollection.Add(bindingElement);
+
+                // 기본인증 허용 
+                ConfigurationElement ftpServerElement = siteElement.GetChildElement("ftpServer");
+                ConfigurationElement securityElement = ftpServerElement.GetChildElement("security");
+                ConfigurationElement authenticationElement = securityElement.GetChildElement("authentication");
+                ConfigurationElement basicAuthenticationElement = authenticationElement.GetChildElement("basicAuthentication");
+                basicAuthenticationElement["enabled"] = true;
+
+                // SSL 허용으로 변경
+                ConfigurationElement sslElement = securityElement.GetChildElement("ssl");
+                sslElement["controlChannelPolicy"] = "SslAllow";
+                sslElement["dataChannelPolicy"] = "SslAllow";
+
+                // FTP 폴더 경로 추가
+                ConfigurationElementCollection siteCollection = siteElement.GetCollection();
+                ConfigurationElement applicationElement = siteCollection.CreateElement("application");
+                applicationElement["path"] = @"/";
+                ConfigurationElementCollection applicationCollection = applicationElement.GetCollection();
+                ConfigurationElement virtualDirectoryElement = applicationCollection.CreateElement("virtualDirectory");
+                virtualDirectoryElement["path"] = @"/";
+                virtualDirectoryElement["physicalPath"] = physicalPath;
+                applicationCollection.Add(virtualDirectoryElement);
+                siteCollection.Add(applicationElement);
+                sitesCollection.Add(siteElement);
+
+                serverManager.CommitChanges();
+
+                AddRole(siteName, role);
+            }
+        }
+
+        // 유저 추가
+        /// <summary>
+        /// FTP 접근 가능한 유저 추가
+        /// </summary>
+        /// <param name="siteName">사이트이름</param>
+        /// <param name="role">유저명</param>
+        /// <returns></returns>
+        private static void AddRole(string siteName, string role)
+        {
+            using (ServerManager serverManager = new ServerManager())
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection authorizationSection = config.GetSection("system.ftpServer/security/authorization", siteName);
+                ConfigurationElementCollection authorizationCollection = authorizationSection.GetCollection();
+
+                ConfigurationElement addElement = authorizationCollection.CreateElement("add");
+                addElement["accessType"] = @"Allow";
+                addElement["roles"] = role;
+                addElement["permissions"] = @"Read, Write";
+                authorizationCollection.Add(addElement);
+
+                serverManager.CommitChanges();
+            }
+        }
+    }
+}
